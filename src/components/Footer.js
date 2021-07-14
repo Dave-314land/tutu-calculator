825f485b8731d59ac6bfabe9d36da49c2d7d61d6
Dave-314land <dave.l.piland@gmail.com>
2021-07-14 16:33:43 -0400

    added homage to The Fantasy Footballers Podcast

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,14 @@ function Footer() {
   return (
     <footer className="footer mt-auto py-3 bg-light">
       <div className="container">
-        <span className="text-muted">Powered By Piland&copy; {year}</span>
+        <span className="text-muted">Powered By Piland&copy; {year} </span>
+        <span className="text-muted">
+          {" "}
+          | Inspired by{" "}
+          <a href="https://www.thefantasyfootballers.com/" target="_">
+            The Fantasy Footballers Podcast
+          </a>{" "}
+        </span>
       </div>
     </footer>
   );
